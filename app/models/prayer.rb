4ee373ab4d50668199142b38939263230c84fc2f
Antonio Sanchez <antonio.sanchez.trespalacios@gmail.com>
2020-05-01 09:51:35 +0900

    fixed some problems in models

--- app/models/prayer.rb
+++ app/models/prayer.rb
@@ -1,2 +1,7 @@
 class Prayer < ApplicationRecord
+  has_one :comment, dependent: :destroy
+  has_one :god, through: :comment
+  validates :name, :pray, presence: true
+  has_one_attached :photo
+  validates :photo, presence: true
 end
