165f94ba8fe68855b8293861577f84fad9f64b34
Antonio Sanchez <antonio.sanchez.trespalacios@gmail.com>
2020-05-01 09:59:51 +0900

    problem with cards fixed

--- app/models/prayer.rb
+++ app/models/prayer.rb
@@ -3,5 +3,5 @@ class Prayer < ApplicationRecord
   has_one :god, through: :comment
   validates :name, :pray, presence: true
   has_one_attached :photo
-  validates :photo, presence: true
+  # validates :photo, presence: true
 end
